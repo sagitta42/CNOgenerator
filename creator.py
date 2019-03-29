dedd849aa3be43065e8a3b4ead06a8ef728bf701
sagitta42 <sssagitta@gmail.com>
2019-03-29 13:56:30 +0100

    Penalty option, replaced tabs by spaces

--- creator.py
+++ creator.py
@@ -2,196 +2,214 @@ import os
 from math import ceil
 
 class Submission():
-	def __init__(self, metal, inj, fit, var, nfits_min, nfits_max):
-		self.metal = metal
-		self.inj = inj
-		self.fit = fit
-		self.var = var
-		self.nfits_min = nfits_min
-		self.nfits_max = nfits_max
-		
-		## corr. fitoptions and species lists filenames
-		self.cfgname = 'fitoptions/fitoptions_' + metal + '_' + inj + '_' + var + '.cfg'
-		self.iccname = 'species_list/species_' + fit + '.icc'
-		
-		## corr. histograms inside of the input file
-		self.cnofolder = metal + '_cno_' + inj # e.g. hz_cno_1
-		self.cnohisto = 'cno_' + metal + '_' + inj
-		
-		## folder for everything related to the given configuration
-		self.outfolder = 'fit_' + metal + '_' + inj + '_cno_' + fit + '_' + var 
-		## folder for the output log files
-		self.logfolder = 'logs_' + self.metal + '_' + self.inj + '_cno_' + self.fit + '_' + self.var
-		## our fit filename: "core" name, will have 20 of these each one 500 fits; name of the folder
-		self.fitfolder = 'fits_' + metal + '_' + inj + '_' + 'cno_' + fit + '_' + var
-		self.batchfolder = 'sbatch_' + metal + '_' + inj + '_' + 'cno_' + fit + '_' + var
-		
+    def __init__(self, metal, inj, fit, var, nfits_min, nfits_max, penalty='none'):
+        '''
+        metal (str): 'hz' or 'lz'
+        inj (str): '1' or '0'
+        fit (str): 'free' or 'fixed'
+        var (str): 'npmts_dt1' or 'npmts_dt2'
+        '''
+        self.metal = metal
+        self.inj = inj
+        self.fit = fit
+        self.var = var
+        self.nfits_min = nfits_min
+        self.nfits_max = nfits_max
+        self.penalty = penalty # to be constrained
+        
+        ## corr. fitoptions and species lists filenames
+        self.cfgname = 'fitoptions/fitoptions_' + metal + '_' + inj + '_' + var + '.cfg'
+        pen = '' if self.penalty == 'none' else '_' + self.penalty + 'penalty'
+        self.iccname = 'species_list/species_' + fit + pen + '.icc'
+        
+        ## corr. histograms inside of the input file
+        self.cnofolder = metal + '_cno_' + inj # e.g. hz_cno_1
+        self.cnohisto = 'cno_' + metal + '_' + inj
+        
+        ## folder for everything related to the given configuration
+        comboname = metal + '_' + inj + '_cno_' + fit + pen + '_' + var
+        self.outfolder = 'fit_' + comboname 
+        ## subfolder for the output log files
+        self.logfolder = 'logs_' + comboname
+        ## subfolder for fit.sh files 
+        self.fitfolder = 'fits_' + comboname
+        ## subfolder for sbatch.sh files
+        self.batchfolder = 'sbatch_' + comboname 
+        
 
-	def output(self):
-		# do not create if already exists
-		if os.path.exists(self.outfolder):
-			print 'Folder', self.outfolder, 'already exists!'
-			return True
-			
-		os.mkdir(self.outfolder) # create output folder
-		print 'Folder for given fit parameters setup:', self.outfolder + '/'
-		return False
-		
-		
-	def fitfiles(self, nbatch, input_folder):
-		''' Create .sh fit files for given parameters (20 files) '''
+    def output(self):
+        # do not create if already exists
+        if os.path.exists(self.outfolder):
+            print 'Folder', self.outfolder, 'already exists!'
+            return True
+            
+        os.mkdir(self.outfolder) # create output folder
+        print 'Folder for given fit parameters setup:', self.outfolder + '/'
+        return False
+        
+        
+    def fitfiles(self, nbatch, input_folder):
+        ''' Create .sh fit files for given parameters (20 files) '''
 
-		num_fits = self.nfits_max - self.nfits_min
-		# folder to store the fitfiles
-		os.mkdir(self.outfolder + '/' + self.fitfolder)
-		print '|'
-		print ' - Subfolder for fit files:', self.fitfolder + '/'
-		print '|\t(', self.fitfolder + '_X.sh, X = 0..' + str(int(ceil(num_fits*1./nbatch))) + ' )'
-		print '|\t( each fit = max ' + str(nbatch) + ' lines)'
+        num_fits = self.nfits_max - self.nfits_min
+        # folder to store the fitfiles
+        os.mkdir(self.outfolder + '/' + self.fitfolder)
+        print '|'
+        print ' - Subfolder for fit files:', self.fitfolder + '/'
+        print '|\t(', self.fitfolder + '_X.sh, X = 0..' + str(int(ceil(num_fits*1./nbatch))) + ' )'
+        print '|\t( each fit = max ' + str(nbatch) + ' lines)'
 
-		# folder to store the batch files
-		os.mkdir(self.outfolder + '/' + self.batchfolder)
-		print '|'
-		print ' - Subfolder with corr. batch files:', self.batchfolder + '/'
-		print '|\t(', self.batchfolder + '_X.sh, X = 0..' + str(int(ceil(num_fits*1./nbatch))) +  ' )'
-		print '|\t( each submission = 1 fit*.sh )'
-		
-		# for now instead of 10 000 use 100, not 500 per file but 5 (20 files)
-		fitfile = None 
-#		fitfile = open('.temp','w') # dummy
-	
-		counter = 0
-		for i in range(self.nfits_min, self.nfits_max):
-			
-			## every 5 files do:
-			if(counter % nbatch == 0):
-				if fitfile:
-					prev_name = fitfile.name
-					fitfile.close()
-					make_executable(prev_name) # chmod +x
-			
-				findex = int(counter / nbatch)
-				# will be accessed in self.batch()
-				self.fitname = self.fitfolder + '_' + str(findex) + '.sh'
+        # folder to store the batch files
+        os.mkdir(self.outfolder + '/' + self.batchfolder)
+        print '|'
+        print ' - Subfolder with corr. batch files:', self.batchfolder + '/'
+        print '|\t(', self.batchfolder + '_X.sh, X = 0..' + str(int(ceil(num_fits*1./nbatch))) +  ' )'
+        print '|\t( each submission = 1 fit*.sh )'
+        
+        # for now instead of 10 000 use 100, not 500 per file but 5 (20 files)
+        fitfile = None 
+#        fitfile = open('.temp','w') # dummy
+    
+        counter = 0
+        for i in range(self.nfits_min, self.nfits_max):
+            
+            ## every 5 files do:
+            if(counter % nbatch == 0):
+                if fitfile:
+                    prev_name = fitfile.name
+                    fitfile.close()
+                    make_executable(prev_name) # chmod +x
+            
+                findex = int(counter / nbatch)
+                # will be accessed in self.batch()
+                self.fitname = self.fitfolder + '_' + str(findex) + '.sh'
 
-				fitfile = open(self.outfolder + '/' + self.fitfolder + '/' + self.fitname, 'w')
-				# the fitter thinks it's in the spectralfit folder
-				print >> fitfile, '#!/bin/bash'
-				print >> fitfile, 'EXEC="./spectralfit"'
-				## add to batch file (when we hit a new findex)
-				self.batch(findex)
+                fitfile = open(self.outfolder + '/' + self.fitfolder + '/' + self.fitname, 'w')
+                # the fitter thinks it's in the spectralfit folder
+                print >> fitfile, '#!/bin/bash'
+                print >> fitfile, 'EXEC="./spectralfit"'
+                ## add to batch file (when we hit a new findex)
+                self.batch(findex)
 
-			# the 5th line is the one that will be copied many times for each input file: full or relative path to the input root file
-			inputname = input_folder + '/Sen_' + str(i) + '.root'
-			logname = 'sen_' + str(i) + '_' + self.metal + '_' + self.inj + '_' + 'cno_' + self.fit + '_' + self.var + '.log'
-			
-			print >> fitfile, '$EXEC', inputname, self.cnofolder + '/TFC_sub/Hsub_' + self.cnohisto + '_0_' + self.var, self.cfgname, self.iccname, '2>&1 | tee', self.outfolder + '/' + self.logfolder + '/' + logname
+            # the 5th line is the one that will be copied many times for each input file: full or relative path to the input root file
+            inputname = input_folder + '/Sen_' + str(i) + '.root'
+            logname = 'sen_' + str(i) + '_' + self.metal + '_' + self.inj + '_' + 'cno_' + self.fit + '_' + self.var + '.log'
+            
+            print >> fitfile, '$EXEC', inputname, self.cnofolder + '/TFC_sub/Hsub_' + self.cnohisto + '_0_' + self.var, self.cfgname, self.iccname, '2>&1 | tee', self.outfolder + '/' + self.logfolder + '/' + logname
 
-			counter+=1
-			
-		# last file
-		prev_name = fitfile.name
-		fitfile.close()
-		make_executable(prev_name) # chmod +x
+            counter+=1
+            
+        # last file
+        prev_name = fitfile.name
+        fitfile.close()
+        make_executable(prev_name) # chmod +x
 
-	
-	def cfgfile(self):
-		''' Create a fitoptions file corresponding to the given configuration (if needed) '''
-			## name of the cfg file
-		print 'Fitoptions:', self.cfgname
-		
-		## if file already exists, do nothing
-		if os.path.exists(self.cfgname):
-			return
-			
-		## otherwise generate from a template
-		cfglines = open('templates/fitoptions_template.cfg').readlines()
-		
-		# line 3: fit variable --> always npmts? not npmts_dt1 or dt2?
-		# cfglines[2] = 'fit_variable = ' + var
-		
-		# line 13: fit variable MC
-		cfglines[12] = 	'fit_variable_MC = ' + self.var + '\n'
+    
+    def cfgfile(self):
+        ''' Create a fitoptions file corresponding to the given configuration (if needed) '''
+            ## name of the cfg file
+        print 'Fitoptions:', self.cfgname
+        
+        ## if file already exists, do nothing
+        if os.path.exists(self.cfgname):
+            return
+            
+        ## otherwise generate from a template
+        cfglines = open('templates/fitoptions_template.cfg').readlines()
+        
+        # line 3: fit variable --> always npmts? not npmts_dt1 or dt2?
+        # cfglines[2] = 'fit_variable = ' + var
+        
+        # line 13: fit variable MC
+        cfglines[12] =     'fit_variable_MC = ' + self.var + '\n'
 
-		# line 82: user data
-		cfglines[81] = 'user_data_hist_name = ' + self.cnofolder + '/User_Data/user_data_' + self.cnohisto  + '_0_' + self.var + '\n'
-		
-		# line 83: user ref
-		cfglines[82] = 'user_ref_hist_name = multivariate/multivariate_lkl_%d_' + self.var + '\n'
-		
-		# line 90: complementary histo
-		cfglines[89] = 'complementary_histo_name = ' + self.cnofolder + '/TFC_tagged/Htag_' + self.cnohisto + '_0_' + self.var + '\n'
-		
-		# line 91: r dist
-		cfglines[90] = 'rdist_data_hist_name = ' + self.cnofolder + '/Rdist_Data/rdist_data_' + self.cnohisto + '_0_' + self.var + '\n'
-		
-		# line 92: rdist ref
-		cfglines[91] = 'rdist_ref_hist_name = multivariate/multivariate_rdist_%d_' + self.var + '\n'
+        # line 82: user data
+        cfglines[81] = 'user_data_hist_name = ' + self.cnofolder + '/User_Data/user_data_' + self.cnohisto  + '_0_' + self.var + '\n'
+        
+        # line 83: user ref
+        cfglines[82] = 'user_ref_hist_name = multivariate/multivariate_lkl_%d_' + self.var + '\n'
+        
+        # line 90: complementary histo
+        cfglines[89] = 'complementary_histo_name = ' + self.cnofolder + '/TFC_tagged/Htag_' + self.cnohisto + '_0_' + self.var + '\n'
+        
+        # line 91: r dist
+        cfglines[90] = 'rdist_data_hist_name = ' + self.cnofolder + '/Rdist_Data/rdist_data_' + self.cnohisto + '_0_' + self.var + '\n'
+        
+        # line 92: rdist ref
+        cfglines[91] = 'rdist_ref_hist_name = multivariate/multivariate_rdist_%d_' + self.var + '\n'
 
-		# line 103: dark noise
-		cfglines[102] = 'dark_noise_window = win' + self.var[-1] + '\n'
-		
-		## save file
-		outfile = open(self.cfgname, 'w')
-		outfile.writelines(cfglines)
-		outfile.close()
-		print '\tcreator.py: cfgfile : generated'
-
-
-	def iccfile(self):
-		''' Create a species list corresponding to the given configuration (if needed) '''
-		print 'Species list:', self.iccname
-		## if file already exists, do nothing
-			
-		if os.path.exists(self.iccname):
-			return
-			
-		## otherwise generate from a template
-		icclines = open('templates/species_list_template.icc').readlines()
-
-		# line 21: cno fixed or free
-		icclines[20] = CNOICC[self.fit]
-		
-		## save file
-		outfile = open(self.iccname, 'w')
-		outfile.writelines(icclines)
-		outfile.close()
-		print '\tcreator.py: iccfile : generated'
+        # line 103: dark noise
+        cfglines[102] = 'dark_noise_window = win' + self.var[-1] + '\n'
+        
+        ## save file
+        outfile = open(self.cfgname, 'w')
+        outfile.writelines(cfglines)
+        outfile.close()
+        print '\tcreator.py: cfgfile : generated'
 
 
+    def iccfile(self):
+        ''' Create a species list corresponding to the given configuration (if needed) '''
+        print 'Species list:', self.iccname
+        ## if file already exists, do nothing
+            
+        if os.path.exists(self.iccname):
+            return
+            
+        ## otherwise generate from a template
+        icclines = open('templates/species_list_template.icc').readlines()
 
-	def logfiles(self):
-		''' Create output folder for the log files '''
-		os.mkdir(self.outfolder + '/' + self.logfolder)
-		print '|'
-		print ' - Subfolder for future log files:', self.logfolder + '/'
-		print '|\t( sen_X_' + self.metal + '_' + self.inj + '_' + 'cno_' + self.fit + '_' + self.var + '.log, X = ' + str(self.nfits_min) + '..' + str(self.nfits_max) + ' )'
+        # line 21: cno fixed or free
+        icclines[20] = CNOICC[self.fit]
+
+        # extra: free (default) or penalty
+        if self.penalty != 'none':
+            line_num, line = ICC[self.penalty]
+            icclines[line_num] = line
+        
+        ## save file
+        outfile = open(self.iccname, 'w')
+        outfile.writelines(icclines)
+        outfile.close()
+        print '\tcreator.py: iccfile : generated'
 
 
-	def batch(self, findex):
-		''' Create jureca batch submission files '''
-			
-		batchname = self.outfolder + '/' + self.batchfolder + '/' + self.batchfolder + '_' + str(findex) + '.sh'
-		batchfile = open(batchname, 'w')
 
-		## batch file template
-		sublines = open('templates/sbatch_submission_template.sh').readlines()
-		
-		# last line: our fit
-		# will submit sbatch being located in the fitter folder, so it will call relative to that position; and the fitter being called will also be relative spectral fitter core folder
-		sublines[-1] = 'srun ' + self.outfolder + '/' +  self.fitfolder + '/' + self.fitname
-		
-		## save
-		batchfile.writelines(sublines)
-		batchfile.close()
-		make_executable(batchname) # chmod +x
+    def logfiles(self):
+        ''' Create output folder for the log files '''
+        os.mkdir(self.outfolder + '/' + self.logfolder)
+        print '|'
+        print ' - Subfolder for future log files:', self.logfolder + '/'
+        print '|\t( sen_X_' + self.metal + '_' + self.inj + '_' + 'cno_' + self.fit + '_' + self.var + '.log, X = ' + str(self.nfits_min) + '..' + str(self.nfits_max) + ' )'
+
+
+    def batch(self, findex):
+        ''' Create jureca batch submission files '''
+            
+        batchname = self.outfolder + '/' + self.batchfolder + '/' + self.batchfolder + '_' + str(findex) + '.sh'
+        batchfile = open(batchname, 'w')
+
+        ## batch file template
+        sublines = open('templates/sbatch_submission_template.sh').readlines()
+        
+        # last line: our fit
+        # will submit sbatch being located in the fitter folder, so it will call relative to that position; and the fitter being called will also be relative spectral fitter core folder
+        sublines[-1] = 'srun ' + self.outfolder + '/' +  self.fitfolder + '/' + self.fitname
+        
+        ## save
+        batchfile.writelines(sublines)
+        batchfile.close()
+        make_executable(batchname) # chmod +x
 
 
 def make_executable(path):
-	''' chmod +x the file'''
-	mode = os.stat(path).st_mode
-	mode |= (mode & 0o444) >> 2    # copy R bits to X
-	os.chmod(path, mode)
+    ''' chmod +x the file'''
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
 
 CNOICC = {'fixed': '   { "nu(CNO)",      -1,   kCyan,   kSolid,  2,    0.,   "fixed", 0.,  50. },\n',
             'free': '{ "nu(CNO)",      -1,   kCyan,   kSolid,  2,    5.36,   "free", 0.,  50. },\n'}
+
+ICC = {'Bi210': [27, '{ "Bi210",        -1,   kSpring, kSolid,  2,    17.5,    "penalty",  17.5,  2.0 },\n']}
--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ from math import ceil
 ## ENTER YOUR CHOICES HERE
 
 # total number of fits
-NFITS_min = 486 # including
+NFITS_min = 0 # including
 NFITS_max = 1000 # not including
 # number of fits per one submission file
 NBATCH = 100
@@ -19,87 +19,98 @@ from creator import *
 
 #----------------------------------------------------------
 
-def generator(metal, inj, fit, var):
-	'''
-	0) metal: metallicity (hz or lz)
-	1) inj: injected CNO (1 or 0)
-	2) fit: how to fit CNO (fixed or free)
-	3) var: energy variable (npmts_dt1, npmts_dt2 or nhits)
-	'''
+def generator(metal, inj, fit, var, penalty):
+    '''
+    0) metal: metallicity (hz or lz)
+    1) inj: injected CNO (1 or 0)
+    2) fit: how to fit CNO (fixed or free)
+    3) var: energy variable (npmts_dt1, npmts_dt2 or nhits)
+    4) penalty: species to put penalty on (e.g. Bi210) or 'none'
+    '''
 
-	## ---------------------------------
-	## technical checks
+    ## ---------------------------------
+    ## technical checks
 
-	if not metal in ['hz','lz']:
-		print '\nOptions for metallicity: hz or lz\n'
-		return
+    if not metal in ['hz','lz']:
+        print '\nOptions for metallicity: hz or lz\n'
+        return
 
-	if not inj in ['0','1']:
-		print '\nOptions for CNO injected: 1 or 0\n'
-		return
+    if not inj in ['0','1']:
+        print '\nOptions for CNO injected: 1 or 0\n'
+        return
 
-	if not fit in ['fixed','free']:
-		print '\nOptions for CNO fit: fixed or free\n'
-		return
+    if not fit in ['fixed','free']:
+        print '\nOptions for CNO fit: fixed or free\n'
+        return
+        
+    if not (penalty in ICC or penalty == 'none'):
+        print '\nOptions for penalty:', ','.join(ICC.keys())
+        return
 
-	## for now ignore nhits
-	if not var in ['npmts_dt1', 'npmts_dt2']:
-		print '\nOptions for energy variable: npmts_dt1 or npmts_dt2 (nhits NOT implemented)\n'
-		return
+    ## for now ignore nhits
+    if not var in ['npmts_dt1', 'npmts_dt2']:
+        print '\nOptions for energy variable: npmts_dt1 or npmts_dt2 (nhits NOT implemented)\n'
+        return
 
 
-	## ---------------------------------
+    ## ---------------------------------
 
-	## init
-	s = Submission(metal, inj, fit, var, NFITS_min, NFITS_max)
-	
-	print # readability
-	print '#######################################'
-	print
-	
-	## folder for given configuration (for submission files and output log file folder)
-	outfolder_exists = s.output()
+    ## init
+    s = Submission(metal, inj, fit, var, NFITS_min, NFITS_max, penalty)
+    
+    print # readability
+    print '#######################################'
+    print
+    
+    ## folder for given configuration (for submission files and output log file folder)
+    outfolder_exists = s.output()
 
-	if(outfolder_exists):
-		print
-		print '#######################################'
-		return
-	
-	
-	## main .sh fit files: split 10000 fits into files of 500 fits (-> 20 files)
-	## also batch files (in parallel)
-	s.fitfiles(NBATCH, INPUT)
-	
-	## output folder for the log files
-	s.logfiles()
+    if(outfolder_exists):
+        print
+        print '#######################################'
+        return
+    
+    
+    ## main .sh fit files: split 10000 fits into files of 500 fits (-> 20 files)
+    ## also batch files (in parallel)
+    s.fitfiles(NBATCH, INPUT)
+    
+    ## output folder for the log files
+    s.logfiles()
 
-	print # readability
-	
-	## corresponding cfg file
-	if not os.path.exists('fitoptions'):
-		os.mkdir('fitoptions')
-	
-	s.cfgfile() # create cfg file if needed
+    print # readability
+    
+    ## corresponding cfg file
+    if not os.path.exists('fitoptions'):
+        os.mkdir('fitoptions')
+    
+    s.cfgfile() # create cfg file if needed
 
-	## corresponding species list
-	if not os.path.exists('species_list'):
-		os.mkdir('species_list')
+    ## corresponding species list
+    if not os.path.exists('species_list'):
+        os.mkdir('species_list')
 
-	s.iccfile() # create species list if needed
+    s.iccfile() # create species list if needed
 
-	print #readability
-	print '#######################################'
-	print
+    print #readability
+    print '#######################################'
+    print
 
 
 
 if __name__ == '__main__':
-	params = sys.argv[1:]
-	
-	if len(params) != 4:
-		print
-		print 'Example: python generator.py hz 0 free npmts_dt1'
-		print
-		sys.exit(1)
+    params = sys.argv[1:]
+    
+    if not len(params) in [4,5]:
+        print
+        print 'Example: python generator.py hz 0 free npmts_dt1'
+        print 'Optional: python generator.py hz 0 free npmts_dt1 penalty=Bi210'
+        print
+        sys.exit(1)
 
-	generator(*params)
+    if 'penalty' in params[-1]:
+        params[-1] = params[-1].split('=')[1]
+    else:
+        params.append('none')
+
+    generator(*params)
